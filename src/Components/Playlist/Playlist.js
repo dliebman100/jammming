1bf55fa64b51e3fa61f01183ba377f28c8e0bc3a
Daniel Liebman <dliebman100@gmail.com>
2021-09-10 14:44:31 -0400

    commented out TrackList no props yet

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,3 +1,17 @@
 import React from "react";
 
 import "./Playlist.css";
+
+class Playlist extends React.Component {
+  render() {
+    return (
+      <div class="Playlist">
+        <input defaultValue={"New Playlist"} />
+        {/* <TrackList /> */}
+        <button class="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>
+    );
+  }
+}
+
+export default Playlist;
