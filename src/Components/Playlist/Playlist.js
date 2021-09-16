e81382525de4932d09cdb9cf183d10ab4cf079b3
Daniel Liebman <dliebman100@gmail.com>
2021-09-16 14:48:14 -0400

    passed event in the input tag to onNameChange prop

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,10 +3,19 @@ import TrackList from "../TrackList/TrackList";
 import "./Playlist.css";
 
 class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.props.onNameChange(event.target.value);
+  }
   render() {
     return (
       <div class="Playlist">
-        <input defaultValue={"New Playlist"} />
+        <input defaultValue={"New Playlist"} onChange={this.handleNameChange} />
         <TrackList
           tracks={this.props.playlistTracks}
           onRemove={this.props.onRemove}
